bc149857a5404713f10c3484703da77142dcb5e2
Sunny Tseng <sunnyyctseng@gmail.com>
2024-09-05 01:35:16 +0000

    updated the table

--- R/shinyApp.R
+++ R/shinyApp.R
@@ -1,34 +1,37 @@
 
+# preset ------------------------------------------------------------------
+
+library(shiny)
+library(dplyr)
+library(DT)
+library(tuneR)
+
+# Function to play audio based on filepath, start, and end columns
+play_audio <- function(filepath, start, end) {
+  song <- readWave(filepath, from = start - 3, to = end + 3, units = "seconds")
+  play(song)
+}
+
 
 # ui ----------------------------------------------------------------------
 
-library(shiny)
-library(DT)
-
-# Define UI
 ui <- page_sidebar(
   
   ## add title
   title = "Bird Audio Validation",
   
   sidebar = sidebar(
-
-    card(
-      card_header("Input"),
-      
-      ## input: validation_file
-      fileInput("detection_list", "Choose the detection file", multiple = FALSE, 
-                accept = c("text/csv", 
-                           "text/comma-separated-values,text/plain",
-                           ".csv")),
-     
-      ## input: species_name
-      selectInput("species_filter", "Choose species:", choices = NULL, selected = NULL) 
-      
-    ),
+    ## input: validation_file
+    fileInput("detection_list", "Choose the detection file", multiple = FALSE, 
+              accept = c("text/csv", 
+                         "text/comma-separated-values,text/plain",
+                         ".csv")),
     
     ## button: save_changes
-    actionButton("save_changes", "Save Changes")
+    actionButton("save_changes", "Save Changes"),
+    
+    ## output: species_list_to_do
+    tableOutput("species_list_to_do")
   ),
   
   ## output: editable_table
@@ -37,21 +40,8 @@ ui <- page_sidebar(
 )
 
 
-
-
-
-
-
 # server  -----------------------------------------------------------------
 
-library(shiny)
-library(dplyr)
-library(DT)
-library(tuneR)
-
-
-
-# Define server logic
 server <- function(input, output, session) {
   
   ## read in the file and save it as an reactive object
@@ -62,7 +52,6 @@ server <- function(input, output, session) {
     ## import dataset
     tryCatch({
       read_csv(input$detection_list$datapath)
-      
     }, error = function(e) {
       # return a safeError if a parsing error occurs
       stop(safeError(e))
@@ -70,33 +59,72 @@ server <- function(input, output, session) {
   })
   
   
-  ## populate the species filter dropdown
+  ## populate the species that still need to be done
+  #! make it filter the species that haven't been processed
   observe({
-    
-    req(input$detection_list)
-    
+    ## make the species list from the imported dataset
     species_choices <- datatable_main() %>%
       select(common_name) %>%
       distinct() %>%
       arrange(common_name) %>%
       pull()
-
-    updateSelectInput(session = session,
-                      inputId = "species_filter",
-                      choices = species_choices,
-                      selected = head(species_choices, 1))
+    
+    output$species_list_to_do <- renderTable(
+      tibble(species = species_choices)
+    )
   })
   
   ## print out the datatable
   output$editable_table <- renderDataTable(
-    datatable_main() %>%
-      datatable(editable = "column", 
-                escape = FALSE, selection = "none")
+    datatable_main() %>% mutate(`Play Audio` = '<button class="play-audio">Play Audio</button>'),
+    editable = "column",
+    filter = "top",
+    escape = FALSE, 
+    selection = "none",
+    rownames = FALSE,
+    options = list(pageLength = 8)
   )
-
   
   
-#### ChatGPT frame
+  ## update the reactive object (datatable_main()) with edited column
+  #! This is not working now!
+  observeEvent(input$editable_table_cell_edit, {
+    datatable_main() <<- editData(datatable_main(), input$editable_table_cell_edit, 'editable_table')
+  })
+  
+  
+  ## Event listener for the play-audio button click
+  observeEvent(input$editable_table_click, {
+    
+    info <- input$editable_table_click
+    
+    if (is.null(info$value) || info$col != ncol(filtered_data())) return()  # Check if the Play Audio button was clicked
+    
+    # Retrieve the file path and start/end times for the selected row
+    selected_row <- datatable_main()[info$row, ]
+    filepath <- selected_row$filepath
+    start <- selected_row$start
+    end <- selected_row$end
+    
+    # Play the audio file
+    if (file.exists(filepath)) {
+      song <- readWave(filepath, from = start - 3, to = end + 3, units = "seconds")
+      play(song, ... = "/play /close")
+    } else {
+      showNotification("Audio file not found.", type = "error")
+    }
+  })
+  
+  
+  ## save changes back to the CSV file
+  observeEvent(input$save_changes, {
+    write.csv(datatable_main(), "test.csv", row.names = FALSE)
+    showNotification("Changes saved successfully.", type = "message")
+  })
+  
+  
+  
+#### ChatGPT framework
   
 # 
 #   # Reactive value to store the data
