3526614e71cef8d0a5a9d6f3d6eccba07274b379
Sunny Tseng <sunnyyctseng@gmail.com>
2024-11-12 13:23:47 -0800

    updated the validation folder code

--- R/Ch4_6_create_validation_folder.R
+++ R/Ch4_6_create_validation_folder.R
@@ -8,3 +8,60 @@ library(here)
 # import data -------------------------------------------------------------
 
 load(here("data", "BirdNET_detections", "bird_data_cleaned_target.RData"))
+
+# function for moving recordings to a given folder
+move_recording <- function(id, filepath, start, end, 
+                           buffer = 3, path = species_folder, ...){
+  
+  # load audio file
+  audio <- readWave(filepath)
+  # Trim the audio using start and end times in seconds
+  trimmed_audio <- cutw(audio, f = audio@samp.rate, 
+                        from = max(0, start - buffer), 
+                        to =  min(length(audio@left) / audio@samp.rate, end + buffer), 
+                        output = "Wave")
+  # Save the trimmed audio if needed
+  writeWave(trimmed_audio, file.path(path, 
+                                     paste0(target_species, "_", id, ".wav")))
+  
+}
+
+
+# isolate recording segments for validation -------------------------------
+
+set.seed(2024)
+
+species <- bird_data_cleaned_target %>%
+  pull(common_name) %>% 
+  unique()
+
+#for (target_species in species) {
+
+
+target_species <- species[1]
+
+  # manage species folder
+  species_folder <- here("data", "validation_recordings", target_species)
+  
+  if (!dir.exists(species_folder)) {
+    dir.create(species_folder, recursive = TRUE)
+  }
+  
+  # validation table
+  table <- bird_data_cleaned_target %>%
+    filter(common_name == target_species) %>%
+    mutate(category = cut(confidence, breaks = seq(0.1, 0.95, by = 0.05))) %>%
+    slice_sample(n = 20, by = category) 
+  
+  write_csv(table, file.path(species_folder, paste0(target_species, "_validation.csv")))
+  
+  # move recordings
+  pmap(table, move_recording)          
+#}
+
+
+
+
+
+
+
